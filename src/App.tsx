ffc3cd8e9802fec3b9d3c18f0a212d687b3d2313
Masataka Shinke <masataka@shinke.com>
2024-08-27 06:03:19 +0900

    log

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
+import bysaLogo from './assets/by-sa.svg'
+import c4bLogo from '/c4b.svg'
 
 function App() {
  
   return (
     <>
+      <div>
+        <img src={c4bLogo} className="logo" alt="Code for Bihoku logo" />
+      </div>
       <h1>Code for Bihoku</h1>
+      <p className="read-the-docs">オープンデータ・テクノロジーで街づくり。</p>
+      <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.ja" target="_blank">
+        <img src={bysaLogo} className="logo" alt="Code for Bihoku logo" />
+      </a>
     </>
   )
 }

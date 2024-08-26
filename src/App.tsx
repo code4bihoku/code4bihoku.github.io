e9887fcb6b29085911b354a492cf1b6ad03d36f3
Masataka Shinke <masataka@shinke.com>
2024-08-27 06:59:18 +0900

    github logo add

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import bysaLogo from './assets/by-sa.svg'
+import githubLogo from './assets/github-mark-white.svg'
 import c4bLogo from '/c4b.svg'
 
 function App() {
@@ -11,8 +12,11 @@ function App() {
       </div>
       <h1>Code for Bihoku</h1>
       <p className="read-the-docs">オープンデータ・テクノロジーで街づくり。</p>
+      <a href="https://github.com/code4bihoku" target="_blank">
+       <img src={githubLogo} className="logo2" alt="github logo" />
+      </a>
       <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.ja" target="_blank">
-        <img src={bysaLogo} className="logo2" alt="Code for Bihoku logo" />
+        <img src={bysaLogo} className="logo2" alt="CC by SA 4.0 logo" />
       </a>
     </>
   )

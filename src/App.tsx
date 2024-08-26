da78d7fab85fc38dd32e2f8454808b1dd656be31
Masataka Shinke <masataka@shinke.com>
2024-08-27 08:57:14 +0900

    osm link add

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import bysaLogo from './assets/by-sa.svg'
 import githubLogo from './assets/github-mark-white.svg'
+import osmLogo from './assets/Openstreetmap_logo.svg'
 import youtubeLogo from './assets/YouTube_light_icon_2017.svg'
 import c4bLogo from '/c4b.svg'
 
@@ -18,6 +19,9 @@ function App() {
       </a>
       <a href="https://www.youtube.com/@CodeForBihoku-g7p" target="_self">
         <img src={youtubeLogo} className="logo2" alt="YouTube logo" />
+      </a>
+      <a href="https://www.openstreetmap.org/#map=10/34.8115/132.9318" target="_self" >
+        <img src={osmLogo} className="logo2" alt="OpenStreetMap logo" />
       </a>
       <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.ja" target="_self">
         <img src={bysaLogo} className="logo2" alt="CC by SA 4.0 logo" />

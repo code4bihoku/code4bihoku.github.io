e7ca7eac51723306339c8c6aa621cfbe7dcc6c6e
Masataka Shinke <masataka@shinke.com>
2024-08-27 18:55:34 +0900

    c4b.svg modify

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import bysaLogo from './assets/by-sa.svg'
 import githubLogo from './assets/github-mark-white.svg'
 import osmLogo from './assets/Openstreetmap_logo.svg'
 import youtubeLogo from './assets/YouTube_light_icon_2017.svg'
+import facebookLogo from './sssets/Facebook_Logo_Secondary.svg'
 import c4bLogo from '/c4b.svg'
 
 function App() {
@@ -15,7 +16,10 @@ function App() {
       <h1>Code for Bihoku</h1>
       <p className="read-the-docs">オープンデータ・テクノロジーで広島県備北地域の街づくり。</p>
       <a href="https://github.com/code4bihoku" target="_self">
-       <img src={githubLogo} className="logo2" alt="github logo" />
+        <img src={githubLogo} className="logo2" alt="github logo" />
+      </a>
+      <a>
+        <img src={facebookLogo} className="logo2" alt="FaceBook logo" />
       </a>
       <a href="https://www.youtube.com/@CodeForBihoku-g7p" target="_self">
         <img src={youtubeLogo} className="logo2" alt="YouTube logo" />
